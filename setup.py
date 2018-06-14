05260385dee2f5fa9d88796ef8345fcb8ba0dc05
Kautenja <kautencreations@gmail.com>
2018-06-14 12:12:36 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -2,27 +2,27 @@ from setuptools import setup
 
 
 setup(
-    name='setuptools-git-version',
-    version='1.0.4',
-    url='https://github.com/pyfidelity/setuptools-git-version',
-    author='pyfidelity UG',
-    author_email='mail@pyfidelity.com',
+    name='very-good-setuptools-git-version',
+    version='1.1.0',
+    url='https://github.com/Kautenja/very-good-setuptools-git-version',
+    author='Chritian Kauten',
+    author_email='kautencreations@gmail.com',
     description='Automatically set package version from Git.',
-    license='http://opensource.org/licenses/MIT',
+    license='MIT',
     classifiers=[
         'Framework :: Setuptools Plugin',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
     ],
-    py_modules=['setuptools_git_version'],
+    py_modules=['very_good_setuptools_git_version'],
     install_requires=[
         'setuptools >= 8.0',
     ],
     entry_points="""
         [distutils.setup_keywords]
-        version_format = setuptools_git_version:validate_version_format
+        version_format = very_good_setuptools_git_version:validate_version_format
         [console_scripts]
-        setuptools-git-version = setuptools_git_version:get_git_version
+        very-good-setuptools-git-version = very_good_setuptools_git_version:get_version
     """,
 )
